c1b8ef24070de8a21945a7626ed279526fb2f232
yuanrui01 <xdyrfree@gmail.com>
2022-08-06 16:44:18 +0800

    idea first commit --- hot-fix

--- src/main/java/com/yuanrui/git/GitTest.java
+++ src/main/java/com/yuanrui/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git fffff222f");
         System.out.println("git fffff222f");
+        System.out.println("git fffff222f11111");
     }
 }
