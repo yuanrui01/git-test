76b7adc6ba3a657715b4cd2abe37fa438c9e954f
yuanrui01 <xdyrfree@gmail.com>
2022-08-06 16:48:48 +0800

    idea fourth commit --- master

--- src/main/java/com/yuanrui/git/GitTest.java
+++ src/main/java/com/yuanrui/git/GitTest.java
@@ -9,6 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git fffff222f");
         System.out.println("git fffff222f");
-        System.out.println("git fffff222f11111");
+        System.out.println("git ff3333fff222f11111");
     }
 }
